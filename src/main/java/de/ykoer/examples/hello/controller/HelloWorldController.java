b076e06e4bf264a5ba543c808c4ef3f87636a614
Felix He <felix.he@prudential.com.cn>
2020-06-16 16:03:28 +0800

    add application insights

--- src/main/java/de/ykoer/examples/hello/controller/HelloWorldController.java
+++ src/main/java/de/ykoer/examples/hello/controller/HelloWorldController.java
@@ -1,5 +1,7 @@
 package de.ykoer.examples.hello.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,12 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class HelloWorldController {
+	private static final Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
 
     @RequestMapping(
             method = RequestMethod.GET,
             value = "/ping"
     )
     public String ping() {
+    	logger.info("This is /ping request");
         return "Hello World! It is wonderful!";
     }
     
@@ -21,7 +25,18 @@ public class HelloWorldController {
             value = "/greeting"
     )
     public String greeting() {
-        return "This is a greeting";
+    	logger.info("This is /greeting request");
+        return "This is a Greeting";
+    }
+    
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "/hello"
+    )
+    public String error() throws Exception {
+    	logger.info("This is /error request");
+    	throw new Exception("This is an error");
+
     }
 
 
